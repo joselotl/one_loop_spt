a9d44d30e7245bfa58f739b73b2a5e847e7a2704
Josue De-Santiago <joselotl@gmail.com>
2019-05-07 12:41:40 -0500

    Relativistic, first

--- one_loop_spt.py
+++ one_loop_spt.py
@@ -14,7 +14,7 @@ from multiprocessing import Pool
 # Definiendo una funcion que sea el espectro lineal
 def linearpower(q):
     if q<0:
-        print 'k negativa'
+        print('k negativa')
         power = 0.
     elif q < qmin:
         power = power_rate_ini*ana.Phs(q)
@@ -43,7 +43,7 @@ def p22_argument(q,k):
     #x_bis_max = min(0.9999999,(1.+r**2-rmin**2)/(2.*r))
     # I use this limits if I trust the analytical extension of P
     x_bis_min = -1.
-    x_bis_max = min(0.999999,1/(r*2))
+    x_bis_max = min(0.999999,1/(2.*r))
     # We make the integral over x using the interpolated powerspectrum
     xintegral = quad(xargument,x_bis_min,x_bis_max,args=(q,k))[0]
     return xintegral
@@ -52,8 +52,41 @@ def p22(k):
     aux0 = p22_argument(ktabla,k)
     p22_arg = aux0*ptabla
     p22_integral = simps(p22_arg,ktabla)
-    print 'Computing P22 for k = ',k
+    print('Computing P22 for k = ',k)
     p22 = 2./98.*k**2/(4.*np.pi**2)*p22_integral
+    # This last step is to put everything back in CAMB like units
+    p22 = p22*h**3
+    return p22
+
+
+
+def xargument_rel(x,q,k):
+    r = q/k
+    den = 1. + r**2 - 2.*r*x
+    factorx = (6*fnl - 10 + 25*r*(x-r))**2/den**2
+    xargument = linearpower(k*np.sqrt(den))*factorx
+    return xargument
+
+def p22_argument_rel(q,k):
+    r = q/k
+    # The next is to integrate only over the numerical part of P
+    #rmin = qmin/k
+    #rmax = qmax/k
+    #x_bis_min = max(-1.,(1.+r**2-rmax**2)/(2.*r))
+    #x_bis_max = min(0.9999999,(1.+r**2-rmin**2)/(2.*r))
+    # I use this limits if I trust the analytical extension of P
+    x_bis_min = -1.
+    x_bis_max = min(0.999999,1./(2.*r))
+    # We make the integral over x using the interpolated powerspectrum
+    xintegral = quad(xargument_rel,x_bis_min,x_bis_max,args=(q,k))[0]
+    return xintegral
+    
+def p22_rel(k):
+    aux0 = p22_argument_rel(ktabla,k)
+    p22_arg = aux0*ptabla/ktabla**2
+    p22_integral = simps(p22_arg,ktabla)
+    print('Computing P22R for k = ',k)
+    p22 = factort/(16.*np.pi**2)*p22_integral
     # This last step is to put everything back in CAMB like units
     p22 = p22*h**3
     return p22
@@ -82,17 +115,52 @@ def p13(k):
     aux1 = p13_argument(ktabla,k)
     p13_arg = aux1*ptabla
     second_integral = simps(p13_arg,ktabla)
-    print 'Computing P13 for k =',k
+    print('Computing P13 for k =', k)
     p13 = k**2/(2.*np.pi)**2*linearpower(k)*second_integral
     # This last step is to put everything in CAMB like units
-    p13 = p13*h**3
+    # I have to divide over two to get p13 actually
+    # And need to remember to sum p1loop = pl + p22 + 2*p13
+    p13 = p13*h**3/2
     return p13
+
+def p13_relativistic_argument(q, k):
+    """
+    We prefer to compute the P13 integral in terms of q instead of r in order to evaluate P_linear in its computed points
+    without an interpolation.
+    """
+    r = q/k
+    factor = 1./(k*q)**2 * (-gnl + 5./9*fnl + 25./54) - 1./6. * (1 + 2*r**2) / q**4 * (-gnl + 10./3*fnl - 50./27)
+    return factor
+
+
+def p13_relativistic(k):
+    aux = p13_relativistic_argument(ktabla, k)
+    p13_arg = aux * ptabla * ktabla**2
+    second_integral = simps(p13_arg,ktabla)
+    print('Computing P13R for k =', k)
+    # The decimal factor is exact 375/40, it needs to be multiplied by 2 to
+    # compute the one loop
+    p13r = 9.375/(2.*np.pi)**2*linearpower(k) * second_integral * factort
+    # This last step is to put everything in CAMB like units
+    p13r = p13r*h**3
+    return p13r
 
 """
 Computing one loop correciton of the matter large scale structure power spectrum.
 Several cosmological constants are defined in analyticpower.py, go there if you want to change them.
 """
 h = 0.6790
+# factor1 corresponds to the dependence of p13 in factors of H and D+
+gnl = 0
+fnl = 0
+# Using H0 in units of inverse Mpc, (Not h/Mpc)
+H0 = h/2997.92458
+z=0
+omm = 1- analyticpower.Omega_lambda
+oml = analyticpower.Omega_lambda
+f1 = omm**(5/9)
+factor_hedda = (5/2/(f1 + 3/2*omm))**2
+factort = factor_hedda * H0**4*(omm*(1+z)+oml/(1+z)**2)**2
 
 # Initializing both numerical and analyic linear powerspectrum
 # the function linearpower uses both at appropiate regions
@@ -114,16 +182,21 @@ power_rate_end = ptabla[-1]/ana.Phs(ktabla[-1])
 # Computing the one-loop corrections
 p13_argument = np.vectorize(p13_argument)
 p22_argument = np.vectorize(p22_argument)
+p22_argument_rel = np.vectorize(p22_argument_rel)
+p13_relativistic_argument = np.vectorize(p13_relativistic_argument)
+
     
 if __name__ == '__main__':
 
-    p = Pool(4)
+    p = Pool(8)
+    p22r_tabla = p.map(p22_rel,ktabla)
     p22_tabla = p.map(p22,ktabla)
     p13_tabla = p.map(p13,ktabla)
+    p13r_tabla = p.map(p13_relativistic,ktabla)
 
     # This last step is to put everything back in CAMB-like units
     ptabla = ptabla*h**3
     ktabla = ktabla/h
-    np.savetxt("./oneloop_Pk.dat",np.transpose([ktabla,ptabla,p22_tabla,p13_tabla]))
+    np.save('output_lcdm',[ktabla, ptabla, p22_tabla, p13_tabla, p22r_tabla, p13r_tabla])
 
 
